8462f9e257bfc627c7d69dab8d34faacedf6244f
Jens Böckmann <jensboe@outlook.de>
2026-08-09 13:13:05 +0200

    Introduce async loading

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
 };
